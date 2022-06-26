fd59a96d0cf9e605b112b593386b178d45d015ab
Viktor Grebenaroski <viktorgrebenaroski@gmail.com>
2022-06-26 15:42:42 +0200

    update knex

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,8 @@ module.exports = {
       host: process.env.DB_HOST,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT
     },
     migrations: {
       directory: './db/migrations'
@@ -28,9 +29,6 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT
-    },
-    migrations: {
-      directory: './db/migrations'
     }
   }
 
